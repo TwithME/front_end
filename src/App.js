d64a4a3a1db69b8e1efcd2a4ef9728c2136bf242
happppi <102276917+hhbb0081@users.noreply.github.com>
2023-11-23 14:06:49 +0900

    Feat: 소셜 로그인 화면 추가

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
+import { Navigate, Route, Routes } from "react-router-dom";
+import LoginPage from "../src/pages/auth/login/login.container";
 import './App.css';
+
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+        {/* 메인페이지 */}
+        <Route path="/login" element={<LoginPage />} />
+        {/* 없는 경로로 갈 경우 로그인 페이지로 강제 이동 */}
+        <Route path="/*" element={<Navigate to="/login"></Navigate>}></Route>
+      </Routes>
     </div>
   );
 }
