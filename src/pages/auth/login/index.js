2b5f530ab2b92dafe96dc95f2eb8e62551184581
happppi <102276917+hhbb0081@users.noreply.github.com>
2023-11-23 15:01:31 +0900

    Feat: 카카오 로그인 추가

---
+++ src/pages/auth/login/index.js
@@ -0,0 +1,13 @@
+
+import Oauth from '@/components/commons/Oauth/Oauth';
+
+export default function login() {
+  return (
+    <>
+      {/* <Layout login /> */}
+      <div>
+        <Oauth/>
+      </div>
+    </>
+  );
+};
